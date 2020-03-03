862d66fc0eb84cf9fa13f64b896cdfcd2208d6c7
Дмитрий <59808523+keinter123@users.noreply.github.com>
2020-03-03 19:50:06 +0200

    Add files via upload

---
+++ homework-9/main.js
@@ -0,0 +1,33 @@
+(function(){
+  let form = document.getElementById('form');
+  let listUL = document.getElementById('list');
+  let input = document.getElementById('input-list');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    if(input.value) {
+      listUL.innerHTML+= '<li>' + input.value + '</li>';
+    store();
+    }
+    input.value = '';
+  });
+
+  listUL.addEventListener('click', function(event) {
+    let target = event.target;
+      if(target.classList.contains('listUL')) {
+        target.classList.add('checked');
+      } else {
+        listUL.removeChild(target)
+      }
+      store();
+    
+});
+  function store() {
+    window.localStorage.myitems = listUL.innerHTML;
+  }
+  function getValues() {
+    let storedValues = window.localStorage.myitems;
+    listUL.innerHTML = storedValues;
+  }
+  getValues();
+}) ();
